0e6e5d7cc14234962cb187b6ec16c12e28a38f44
Lightfooted <vesselofbalance@hotmail.com>
2021-11-12 01:10:34 -0500

    fixed line 12. was getting error data was being called before define

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -9,8 +9,8 @@ import { REMOVE_BOOK } from '../utils/mutations';
 
 
 const SavedBooks = () => {
-  const userData = data?.me || {};
   const { loading, data } = useQuery(GET_ME);
+  const userData = data?.me || {};
   const [removeBook] = useMutation(REMOVE_BOOK);
    
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
