b5a1722562e43c5241c21164842c091c947960f1
Lightfooted <vesselofbalance@hotmail.com>
2021-11-11 22:33:38 -0500

    double checking typeDef because of errors. vsc code says there is a whitespace difference. file looks the same to me. might be a bug.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -26,7 +26,7 @@ const typeDefs = gql`
     image: String
     link: String
   }
-
+ 
   type Auth {
     token: ID!
     user: User
@@ -35,7 +35,7 @@ const typeDefs = gql`
   type Query {
     me: User
   }
-
+  
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
